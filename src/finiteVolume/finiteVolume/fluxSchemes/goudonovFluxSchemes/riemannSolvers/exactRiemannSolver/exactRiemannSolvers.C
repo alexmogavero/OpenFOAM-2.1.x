c5aebcab05e11dd163717ca91e7c57f534f8c16c
Alessandro Mogavero <alessandro.mogavero@strath.ac.uk>
2013-08-24 12:12:18 +0000

    The category goudonovFluxSchemes has been added. The goudonovFluxScheme and the wasFluxSchemes have been moved in this category. Also the reinmannSolvers have been moved here.

---
+++ src/finiteVolume/finiteVolume/fluxSchemes/goudonovFluxSchemes/riemannSolvers/exactRiemannSolver/exactRiemannSolvers.C
@@ -0,0 +1,28 @@
+
+#include "exactRiemannSolver.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace fv
+{
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// Define the constructor function hash tables
+
+defineTypeNameAndDebug(exactRiemannSolver, 0);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace fv
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
